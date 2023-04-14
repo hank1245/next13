2a61b5bd8211b770d34dfc8a2bd1a07ed983a033
Hank Kim <hank29206880@gmail.com>
2023-04-14 15:20:32 +0900

    initial commit

---
+++ app/about/team/page.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function TeamPage() {
+  return (
+    <div>
+      <h1>Our team</h1>
+      <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Incidunt qui
+        animi ipsa harum quaerat, dicta consequuntur provident enim possimus
+        veritatis iure, eos sint nihil sequi at aperiam ad molestiae rerum quos
+        atque, aliquam hic. Animi aspernatur tempora esse illo exercitationem
+        natus, obcaecati facilis id ad dolorum, quia nisi doloremque? Ex!
+      </div>
+    </div>
+  );
+}
+
+export default TeamPage;
